58d0166c3af917fdcb73234a11f230f2006d45f2
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 12:58:12 +0530

    Update favoriteMovieRoutes.js

--- routes/favoriteMovieRoutes.js
+++ routes/favoriteMovieRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticateUser } from "../middleware/authMovieMiddleware.js";
+import { authenticateUser } from "../middleware/movieAuthMiddleware.js";
 import {addFavorite, deleteFavorite, getFavorites} from "../controllers/favoriteMovieController.js";
 
 const router = express.Router();
