b2ee84d6ead8601620f506c5ae9301c3ccd36b79
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 10:53:14 +0530

    Update Favorite.js

--- models/Favorite.js
+++ models/Favorite.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const favoriteSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-    movieCode: {type: String, required: true},
-    movieName: {type: String},
-});
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  movieId: { type: Number, required: true },
+  title: String,
+  poster_path: String,
+  release_date: String,
+  vote_average: Number,
+}, { timestamps: true });
 
 export default mongoose.model("Favorite", favoriteSchema);
