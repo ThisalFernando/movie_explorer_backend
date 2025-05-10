298a9e045f6c109faf67f509401a0b020e8b73f7
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 12:37:16 +0530

    Update UserMovie.js

--- models/UserMovie.js
+++ models/UserMovie.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userMovieSchema = new mongoose.Schema({
     name: {type: String, required: true},
     address: {type: String, required: true},
-    email: {type: String, reuired: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     role: {type: String, enum: ["user", "admin"], default: "user"}
 }, {timestamps: true});
