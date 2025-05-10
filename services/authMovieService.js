d6d819413b83ec40a24d19ccb597e7ea1174d276
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 11:55:12 +0530

    Update authMovieService.js

--- services/authMovieService.js
+++ services/authMovieService.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../models/User.js";
+import User from "../models/UserMovie.js";
 import { generateToken } from "../utils/jwtUtils.js";
 
 // Service for register
