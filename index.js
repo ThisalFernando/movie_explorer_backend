d7cd9193789f27372474c7e7944a72056d83e48f
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 15:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://movie-explorer-omega-green.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
