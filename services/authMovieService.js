1fe094f401cbef4bbd15eb3f9a8c964f20a8b282
Thisal Fernando <148759527+ThisalFernando@users.noreply.github.com>
2025-05-10 12:32:27 +0530

    Update authMovieService.js

--- services/authMovieService.js
+++ services/authMovieService.js
@@ -1,21 +1,21 @@
 import bcrypt from "bcryptjs";
-import User from "../models/UserMovie.js";
+import UserMovie from "../models/UserMovie.js";
 import { generateToken } from "../utils/jwtUtils.js";
 
 // Service for register
 export const registerUser = async ({ name, address, email, password }) => {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await UserMovie.findOne({ email });
     if (existingUser) throw new Error("User already exists");
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ name, address, email, password: hashedPassword });
+    const newUser = await UserMovie.create({ name, address, email, password: hashedPassword });
 
     return { user: newUser, token: generateToken(newUser) };
 };
 
 // Service for login
 export const loginUser = async ({ email, password }) => {
-    const user = await User.findOne({ email });
+    const user = await UserMovie.findOne({ email });
     if (!user) throw new Error("Invalid email or password");
 
     const isMatch = await bcrypt.compare(password, user.password);
